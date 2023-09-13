456a169c5a5983ec641c140eede0fbf6f1e547af
Abhay Rajak <mail2rajab01@gmail.com>
2023-09-14 00:04:46 +0530

    first project - initial commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
